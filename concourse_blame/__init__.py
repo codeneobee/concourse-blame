7f0e353ab932f1ef5ff583e06ce8653550169c92
Brix <brixblog@gmail.com>
2019-09-27 16:21:23 +0200

    Play sound if configured

--- concourse_blame/__init__.py
+++ concourse_blame/__init__.py
@@ -1,6 +1,9 @@
 import getpass
 import os
+import random
 import time
+
+from playsound import playsound
 
 from concourse_blame.job import *
 from concourse_blame.tts import TTS
@@ -45,7 +48,35 @@ class ConcourseBlame:
                 continue
 
             # status has changed
+
             latest_job = current_job
-            latest_committer = get_latest_committer(self.config['git']['clone_path'])
             print('Update on job {}: ID {} / status {}'.format(
                 self.config['concourse']['job'], latest_job.id, latest_job.status))
+
+            if latest_job.status not in self.config['sounds']['status']:
+                print('... but no notification has been configured for this status.')
+                continue
+
+            # there is a sound configured to be played for the new status
+
+            latest_committer = get_latest_committer(self.config['git']['clone_path'], self.config['teams'])
+            print('Committer {} in team {}'.format(latest_committer.name, latest_committer.team))
+
+            # start sound
+            if 'start' in self.config['sounds']:
+                playsound(self.config['sounds']['start'])
+            # status sound
+            playsound(self.config['sounds']['status'][latest_job.status])
+            # tts
+            if latest_job.status in self.config['texts']['status']:
+                texts = self.config['texts']['status'][latest_job.status]
+                if len(texts) > 0:
+                    text = random.choice(texts)
+                    if latest_committer.team:
+                        committer_text = self.config['texts']['committer']['from_team'].replace('%name', latest_committer.name).replace('%team', latest_committer.team)
+                    else:
+                        committer_text = self.config['texts']['committer']['no_team'].replace('%name', latest_committer.name)
+                    tts.say(text.replace('%committer', committer_text))
+            # end sound
+            if 'end' in self.config['sounds']:
+                playsound(self.config['sounds']['end'])
--- concourse_blame/job.py
+++ concourse_blame/job.py
@@ -2,13 +2,18 @@ import os
 import re
 
 
-class ConcourseJob:
+class Committer:
 
-    @staticmethod
-    def get_latest(concourse_target: str, concourse_job_name: str):
-        latest_job = os.popen('fly -t {} builds -j {} -c 1'.format(concourse_target, concourse_job_name)).read()
-        columns = re.split('\s+', latest_job)
-        return ConcourseJob(columns[2], columns[3])
+    def __init__(self, name: str, team_config: dict):
+        self.name = name
+        self.team = None
+        for team, members in team_config.items():
+            if name in members:
+                self.team = team
+                return
+
+
+class ConcourseJob:
 
     def __init__(self, id: int, status: str):
         self.id = id
@@ -24,10 +29,10 @@ def get_latest_job(concourse_target: str, concourse_job_name: str) -> ConcourseJ
     return ConcourseJob(columns[2], columns[3])
 
 
-def get_latest_committer(bare_repo_path: str) -> str:
+def get_latest_committer(bare_repo_path: str, team_config: dict) -> Committer:
     return_path = os.getcwd()
     os.chdir(bare_repo_path)
     os.system('git fetch')
-    latest_committer = os.popen("git log -1 --pretty=format:'%an'").read()
+    latest_committer = os.popen("git log -1 --pretty=format:'%an'").read().replace("'", "")
     os.chdir(return_path)
-    return latest_committer
+    return Committer(latest_committer, team_config)
